3aae2f8234317798e964fc23c13471f2af531379
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-06 17:41:58 +0800

    Delete config directory

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Findme::Application.config.secret_token = 'ecaecd7bc30a3c10538185af468b08a36df33f4537b22d53a87f939136482d650665e2da490fd761657458d5a062f81334ca8c5d1543b6f36ed3207e865a915e'
